36f74aa0784e2305b848c1770abfe5764829fa93
autonomus-robotics <36733770+autonomus-robotics@users.noreply.github.com>
2019-01-23 15:09:19 +0300

    Add files via upload

---
+++ GAN_face/gan_3dface.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+class MeshGenerator:
+    def __init__(self, face_image, base_model):
+        self.face_image = face_image
+        self.base_model = base_model
+
+    def construct_model(self):
+
---
+++ GAN_face/gan_settings.py
@@ -0,0 +1,40 @@
+
+
+"""
+ОСНОВНЫЕ НАСТРОЙКИ
+"""
+# РАЗРЕЩЕНИЕ, СТРОКИ  \\  СТОБЦЫ
+_IMG_ROWS = 600
+_IMG_COLS = 600
+_CHANNEL = 3
+
+_TRAIN_IMG_PATH = 'G:\\faces\\img\\person_0\\'
+
+
+
+"""
+НАСТРОЙКИ ОБУЧЕНИЯ
+"""
+_TRAIN_STEPS = 2000
+_BATCH_SIZE = 4
+_SAVE_INTERVAL = 500
+_OUTPUT_IMAGES_X = 3
+_OUTPUT_IMAGES_Y = 3
+_GENERATED_FACES_PATH = 'D:\\PycharmProjects\\GAN_faces\\generated_faces\\'
+
+
+
+
+
+
+
+
+
+
+
+"""
+ИМПОРТИРУЕМЫЕ НАСТРОЙКИ
+"""
+
+# РАЗРЕЩЕНИЕ, СТРОКИ  \\  СТОБЦЫ
+IMG_RES = (_IMG_ROWS, _IMG_COLS)
